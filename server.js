5a929f7dabc551021467d6b4cba106aa36b0770e
Rachananagavelli <rachanareddynagavelli@gmail.com>
2023-09-17 18:34:57 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -1,32 +1,23 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
 const port = 3000;
 
 app.use('/',express.static('public'));
-
-const budget = {
-    myBudget: [
-    {
-        title: "Eat out",
-        budget: 25
-    },
-    {
-        title: "Rent",
-        budget: 375
-    },
-    {
-        title: "Grocery",
-        budget: 110
-    },
-]
-};
 
 app.get('/hello',(req,res) => {
     res.send('Hello World!');
 });
 
 app.get('/budget',(req,res) => {
-    res.json(budget);
+    fs.readFile('budget.json', 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+          res.status(500).send('Error reading JSON file');
+        } else {
+          res.json(JSON.parse(data));
+        }
+      });
 });
 
 app.listen(port, () => {
